f62fadf2d21e9937845499d5f8cf91ff050d48a7
Mehdi Rahal <mehdi@lablabee.com>
2025-05-24 12:08:09 +0100

    Fix 2

--- src/pages/rooms/RoomDetails.jsx
+++ src/pages/rooms/RoomDetails.jsx
@@ -1,40 +1,39 @@
-// import Heading from "@/components/Heading";
-// import BookingForm from "@/components/BookingForm";
-// import Image from "next/image";
-// import Link from "next/link";
 import { FaChevronLeft } from 'react-icons/fa';
-// import getSingleRoom from "@/app/actions/getSingleRoom";
 import { Link, useParams } from 'react-router-dom';
 import Heading from '../../components/Heading';
 import BookingForm from '../../components/BookingForm';
 import { useEffect, useState } from 'react';
 
-const RoomDetails = ({ baseUrl }) => {
+// @ts-ignore
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+console.log('RoomDetails baseUrl:', baseUrl);
+
+/**
+ * @typedef {Object} Room
+ * @property {string} name
+ * @property {string} image
+ * @property {string} description
+ * @property {number} sqft
+ * @property {string} availability
+ * @property {number} price_per_hour
+ * @property {string} address
+ */
+
+const RoomDetails = () => {
   const params = useParams();
   const { id } = params;
 
+  /** @type {[any, Function]} */
   const [room, setRoom] = useState(null);
 
   useEffect(() => {
     const getRoom = async () => {
       const response = await fetch(`${baseUrl}/rooms/${id}`);
-
       const data = await response.json();
-
       setRoom(data);
     };
-
     getRoom();
-  }, []);
-
-  //   const room = await getSingleRoom(id);
-
-  //   const bucketId = process.env.NEXT_PUBLIC_APPWRITE_STORAGE_BUCKET_ROOMS;
-  //   const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT;
-
-  //   const imageUrl = `https://cloud.appwrite.io/v1/storage/buckets/${bucketId}/files/${room.image}/view?project=${projectId}`;
-
-  //   const imageSrc = room.image ? imageUrl : "/images/no-image.jpg";
+  }, [id]);
 
   if (!room) {
     return <Heading title="Room Not Found" />;
@@ -49,7 +48,6 @@ const RoomDetails = ({ baseUrl }) => {
           <FaChevronLeft className="inline mr-1" />
           <span className="ml-2">Back to Rooms</span>
         </Link>
-
         <div className="flex flex-col sm:flex-row sm:space-x-6">
           <img
             src={room.image}
@@ -58,10 +56,8 @@ const RoomDetails = ({ baseUrl }) => {
             height={100}
             className="w-full sm:w-1/3 h-64 object-cover rounded-lg"
           />
-
           <div className="mt-4 sm:mt-0 sm:flex-1">
             <p className="text-gray-600 mb-4">{room.description}</p>
-
             <ul className="space-y-2">
               <li>
                 <span className="font-semibold text-gray-800">Size:</span> {room.sqft}
@@ -81,7 +77,6 @@ const RoomDetails = ({ baseUrl }) => {
             </ul>
           </div>
         </div>
-
         <BookingForm room={room} />
       </div>
     </>
